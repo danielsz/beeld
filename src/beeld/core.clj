edce99d2c2748f38e554ecd456b61f21fa936bf4
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2025-03-17 04:08:38 +0800

    write method
    - Dual arity

--- src/beeld/core.clj
+++ src/beeld/core.clj
@@ -14,7 +14,8 @@
   (->bytes [x])
   (->base64 [x])
   (detect-image-format [x])
-  (mime-type [x]))
+  (mime-type [x])
+  (write [x] [x folder]))
 
 (extend-protocol Beeld
   File
@@ -29,6 +30,11 @@
   (mime-type [x]
     (Files/probeContentType (.toPath x)))
   (filename [x] (.getName x))
+  (write
+    ([x] (write x (System/getProperty "java.io.tmpdir")))
+    ([x dest]
+     (let [name (filename x)]
+       (write (->bytes x) (str dest "/" name)))))
   URI
   (filesize [x]
     (Long/parseLong (get-in (client/head (str x)) [:headers "Content-Length"])))
@@ -39,6 +45,10 @@
   (filename [x] (filename (io/file (.getPath x))))
   (->bytes [x]
     (:body (client/get (str x) {:as :byte-array})))
+  (write
+    ([x] (write x (System/getProperty "java.io.tmpdir")))
+    ([x folder] (let [name (filename x)]
+                  (write (->bytes x) (str folder "/" name)))))
   String
   (filesize [x]
     (cond
@@ -56,6 +66,12 @@
     (->base64 (->bytes x)))
   (detect-image-format [x]
     (detect-image-format (io/input-stream x)))
+  (write
+    ([x] (write x (System/getProperty "java.io.tmpdir")))
+    ([x dest]
+     (cond
+       (.exists (io/file x)) (write (->bytes (io/file x)) (str dest "/" (filename x)))
+       (URL. x) (write (URL. x) dest))))
   (mime-type [x]
     (when (.exists (io/file x)) (Files/probeContentType (.toPath (io/file x)))))
   byte/1
@@ -63,6 +79,11 @@
     (crypto/encode-base64 x))
   (detect-image-format [x]
     (detect-image-format (io/input-stream x)))
+  (write
+    ([x] (write x (System/getProperty "java.io.tmpdir")))
+    ([x dest]
+     (with-open [w (io/output-stream dest)]
+       (.write w x))))
   BufferedInputStream
   (->bytes [x]
     (with-open [in x
@@ -73,5 +94,9 @@
     (let [in (ImageIO/createImageInputStream x)
         iis (ImageIO/getImageReaders in)]
     (for [reader (iterator-seq iis)]
-      (.getFormatName reader)))))
+      (.getFormatName reader))))
+  (write
+    ([x] (write x (System/getProperty "java.io.tmpdir")))
+    ([x dest]
+     (write (->bytes x) dest))))
 
