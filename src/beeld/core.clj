d50cd11a1d1cea6096eab4107513ea2ef2a87e0a
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2025-04-30 12:09:33 +0800

    scale image

--- src/beeld/core.clj
+++ src/beeld/core.clj
@@ -1,12 +1,15 @@
 (ns beeld.core
   (:require [clojure.java.io :as io]
             [clj-http.client :as client]
-            [kryptos.core :as crypto])
+            [kryptos.core :as crypto]
+            [image-resizer.resize :refer [resize-fn]]
+            [image-resizer.scale-methods :refer [speed]])
   (:import [javax.imageio ImageIO]
            [java.net URI URL]
            [java.io File]
-           [java.io BufferedInputStream]
-           [java.nio.file Files]))
+           [java.io BufferedInputStream ByteArrayOutputStream]
+           [java.nio.file Files]
+           [java.awt.image BufferedImage]))
 
 (defprotocol Beeld
   (filename [x])
@@ -15,7 +18,9 @@
   (->base64 [x])
   (detect-image-format [x])
   (mime-type [x])
-  (write [x] [x folder]))
+  (write [x] [x folder])
+  (scale [x] [x width height] [x width height quality])
+  (clone [is n]))
 
 (extend-protocol Beeld
   File
@@ -35,6 +40,13 @@
     ([x dest]
      (let [name (filename x)]
        (write (->bytes x) (str dest "/" name)))))
+  (scale
+    ([x]
+     (scale x 750 750))
+    ([x width height]
+     (scale x width height speed))
+    ([x width height quality]
+     (scale (io/input-stream x) width height quality) ))
   URI
   (filesize [x]
     (Long/parseLong (get-in (client/head (str x)) [:headers "Content-Length"])))
@@ -49,6 +61,13 @@
     ([x] (write x (System/getProperty "java.io.tmpdir")))
     ([x folder] (let [name (filename x)]
                   (write (->bytes x) (str folder "/" name)))))
+  (scale
+    ([x]
+     (scale x 750 750))
+    ([x width height]
+     (scale x width height speed))
+    ([x width height quality]
+     (scale (:body (client/get (str x) {:as :byte-array})) width height quality)))
   String
   (filesize [x]
     (cond
@@ -62,6 +81,15 @@
     (cond
       (.exists (io/file x)) (->bytes (io/input-stream x))
       (URL. x) (->bytes (URL. x))))
+  (scale
+    ([x]
+     (scale x 750 750))
+    ([x width height]
+     (scale x width height speed))
+    ([x width height quality]
+     (cond
+       (.exists (io/file x)) (scale (io/file x) width height quality)
+       (URL. x) (scale (URL. x) width height quality))))
   (->base64 [x]
     (->base64 (->bytes x)))
   (detect-image-format [x]
@@ -84,6 +112,13 @@
     ([x dest]
      (with-open [w (io/output-stream dest)]
        (.write w x))))
+  (scale
+    ([x]
+     (scale x 750 750))
+    ([x width height]
+     (scale x width height speed))
+    ([x width height quality]
+     (scale (io/input-stream x) width height quality)))
   BufferedInputStream
   (->bytes [x]
     (with-open [in x
@@ -95,6 +130,23 @@
         iis (ImageIO/getImageReaders in)]
     (for [reader (iterator-seq iis)]
       (.getFormatName reader))))
+  (clone [is n]
+    (let [baos (ByteArrayOutputStream.)]
+      (.transferTo is baos)
+      (take n (repeatedly #(io/input-stream (.toByteArray baos))))))
+  (scale
+    ([x]
+     (scale x 750 750))
+    ([x width height]
+     (scale x width height speed))
+    ([x width height quality]
+     (let [[a b] (clone x 2)
+           image-format (first (detect-image-format a))
+           resize-f (resize-fn width height quality)
+           ^BufferedImage resized-image (resize-f b)]
+       (with-open [baos (java.io.ByteArrayOutputStream.)]
+         (ImageIO/write resized-image image-format baos)
+         (.toByteArray baos)))))
   (write
     ([x] (write x (System/getProperty "java.io.tmpdir")))
     ([x dest]
