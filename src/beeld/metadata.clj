616da9ab81360ec08f952618c27b438585020b61
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2025-05-01 13:00:16 +0800

    Macro for exif Tag
    - Reduce boilerplate code

--- src/beeld/metadata.clj
+++ src/beeld/metadata.clj
@@ -8,94 +8,76 @@
            [com.drew.metadata.file FileTypeDirectory]
            [com.drew.metadata.file FileSystemDirectory]))
 
-(defn mime-type [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) FileTypeDirectory)]
-    (description (metadata file) (class directory) FileTypeDirectory/TAG_DETECTED_FILE_MIME_TYPE)))
+(defmacro exif-tag [file-or-is directory tag]
+  `(let [metadata# (metadata ~file-or-is)]
+    (when-let [directory# (.getFirstDirectoryOfType metadata# ~directory)]
+      (description metadata# (class directory#) ~tag))))
 
-(defn filename [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) FileSystemDirectory)]
-    (description (metadata file) (class directory) FileSystemDirectory/TAG_FILE_NAME)))
+(defn mime-type [file-or-is]
+  (exif-tag file-or-is FileTypeDirectory FileTypeDirectory/TAG_DETECTED_FILE_MIME_TYPE))
 
-(defn simulation [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) FujifilmMakernoteDirectory)]
-    (description (metadata file) (class directory) FujifilmMakernoteDirectory/TAG_FILM_MODE)))
+(defn filename [file-or-is]
+  (exif-tag file-or-is FileSystemDirectory FileSystemDirectory/TAG_FILE_NAME))
 
-(defn compression [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifIFD0Directory)]
-    (description (metadata file) (class directory) ExifIFD0Directory/TAG_COMPRESSION)))
+(defn simulation [file-or-is]
+  (exif-tag file-or-is FujifilmMakernoteDirectory FujifilmMakernoteDirectory/TAG_FILM_MODE))
+
+(defn compression [file-or-is]
+  (exif-tag file-or-is ExifIFD0Directory ExifIFD0Directory/TAG_COMPRESSION))
 
 (defn orientation [file-or-is]
-  (let [metadata (metadata file-or-is)]
-    (when-let [directory (.getFirstDirectoryOfType metadata ExifIFD0Directory)]
-      (description metadata (class directory) ExifIFD0Directory/TAG_ORIENTATION))))
+  (exif-tag file-or-is ExifIFD0Directory ExifIFD0Directory/TAG_ORIENTATION))
 
-(defn make [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifIFD0Directory)]
-    (description (metadata file) (class directory) ExifIFD0Directory/TAG_MAKE)))
+(defn make [file-or-is]
+  (exif-tag file-or-is ExifIFD0Directory ExifIFD0Directory/TAG_MAKE))
 
-(defn model [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifIFD0Directory)]
-    (description (metadata file) (class directory) ExifIFD0Directory/TAG_MODEL)))
+(defn model [file-or-is]
+  (exif-tag file-or-is ExifIFD0Directory ExifIFD0Directory/TAG_MODEL))
 
-(defn lens [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifSubIFDDirectory)]
-    (description (metadata file) (class directory) ExifSubIFDDirectory/TAG_LENS)))
+(defn lens [file-or-is]
+  (exif-tag file-or-is ExifSubIFDDirectory ExifSubIFDDirectory/TAG_LENS))
 
-(defn lens-make [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifSubIFDDirectory)]
-    (description (metadata file) (class directory) ExifSubIFDDirectory/TAG_LENS_MAKE)))
+(defn lens-make [file-or-is]
+  (exif-tag file-or-is ExifSubIFDDirectory ExifSubIFDDirectory/TAG_LENS_MAKE))
 
-(defn lens-model [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifSubIFDDirectory)]
-    (description (metadata file) (class directory) ExifSubIFDDirectory/TAG_LENS_MODEL)))
+(defn lens-model [file-or-is]
+  (exif-tag file-or-is ExifSubIFDDirectory ExifSubIFDDirectory/TAG_LENS_MODEL))
 
-(defn aperture [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifSubIFDDirectory)]
-    (description (metadata file) (class directory) ExifSubIFDDirectory/TAG_APERTURE)))
+(defn aperture [file-or-is]
+  (exif-tag file-or-is ExifSubIFDDirectory ExifSubIFDDirectory/TAG_APERTURE))
 
-(defn fnumber [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifSubIFDDirectory)]
-    (description (metadata file) (class directory) ExifSubIFDDirectory/TAG_FNUMBER)))
+(defn fnumber [file-or-is]
+  (exif-tag file-or-is ExifSubIFDDirectory ExifSubIFDDirectory/TAG_FNUMBER))
 
-(defn shutter-speed [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifSubIFDDirectory)]
-    (description (metadata file) (class directory) ExifSubIFDDirectory/TAG_SHUTTER_SPEED)))
+(defn shutter-speed [file-or-is]
+  (exif-tag file-or-is ExifSubIFDDirectory ExifSubIFDDirectory/TAG_SHUTTER_SPEED))
 
-(defn exposure-time [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifSubIFDDirectory)]
-    (description (metadata file) (class directory) ExifSubIFDDirectory/TAG_EXPOSURE_TIME)))
+(defn exposure-time [file-or-is]
+  (exif-tag file-or-is ExifSubIFDDirectory ExifSubIFDDirectory/TAG_EXPOSURE_TIME))
 
-(defn focal-length [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifSubIFDDirectory)]
-    (description (metadata file) (class directory) ExifSubIFDDirectory/TAG_FOCAL_LENGTH)))
+(defn focal-length [file-or-is]
+  (exif-tag file-or-is ExifSubIFDDirectory ExifSubIFDDirectory/TAG_FOCAL_LENGTH))
 
-(defn lens-specification [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifSubIFDDirectory)]
-    (description (metadata file) (class directory) ExifSubIFDDirectory/TAG_LENS_SPECIFICATION)))
+(defn lens-specification [file-or-is]
+  (exif-tag file-or-is ExifSubIFDDirectory ExifSubIFDDirectory/TAG_LENS_SPECIFICATION))
 
-(defn compresion-type [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) JpegDirectory)]
-    (description (metadata file) (class directory) JpegDirectory/TAG_COMPRESSION_TYPE)))
+(defn iso [file-or-is]
+  (exif-tag file-or-is ExifSubIFDDirectory ExifSubIFDDirectory/TAG_ISO_EQUIVALENT))
 
-(defn data-precision [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) JpegDirectory)]
-    (description (metadata file) (class directory) JpegDirectory/TAG_DATA_PRECISION)))
+(defn original-date [file-or-is]
+  (exif-tag file-or-is ExifSubIFDDirectory ExifSubIFDDirectory/TAG_DATETIME_ORIGINAL))
 
-(defn iso [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifSubIFDDirectory)]
-    (description (metadata file) (class directory) ExifSubIFDDirectory/TAG_ISO_EQUIVALENT)))
+(defn description-exif [file-or-is]
+  (exif-tag file-or-is ExifIFD0Directory ExifIFD0Directory/TAG_IMAGE_DESCRIPTION))
 
-(defn caption [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) IptcDirectory)]
-    (description (metadata file) (class directory) IptcDirectory/TAG_CAPTION)))
+(defn compresion-type [file-or-is]
+  (exif-tag file-or-is JpegDirectory JpegDirectory/TAG_COMPRESSION_TYPE))
 
-(defn description-exif [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifIFD0Directory)]
-    (description (metadata file) (class directory) ExifIFD0Directory/TAG_IMAGE_DESCRIPTION)))
+(defn data-precision [file-or-is]
+  (exif-tag file-or-is JpegDirectory JpegDirectory/TAG_DATA_PRECISION))
 
-(defn original-date [file]
-  (when-let [directory (.getFirstDirectoryOfType (metadata file) ExifSubIFDDirectory)]
-    (.getDate directory ExifSubIFDDirectory/TAG_DATETIME_ORIGINAL)))
+(defn caption [file-or-is]
+  (exif-tag file-or-is IptcDirectory IptcDirectory/TAG_CAPTION))
 
 (defn geolocation [file]
   (when-let [directory (.getFirstDirectoryOfType (metadata file) GpsDirectory)]
