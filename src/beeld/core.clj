9da1c4fc1b88d2cd20d92ed2b80cefb383f1ade4
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2026-04-08 12:57:44 +0800

    3-arity write in Beeld protocol
    - And a test suite

--- src/beeld/core.clj
+++ src/beeld/core.clj
@@ -20,140 +20,150 @@
   (->base64 [x])
   (detect-image-format [x])
   (mime-type [x])
-  (write [x] [x folder])
+  (write [x] [x dest] [x n dest])
   (scale [x] [x width height] [x width height quality])
   (clone [is n]))
 
+
+(defn url-string? [x]
+  (try
+    (let [url (URL. x)]
+      (and (seq (.getProtocol url))
+           (seq (.getHost url))))
+    (catch java.net.MalformedURLException _ false)))
+
+
 (extend-protocol Beeld
   File
-  (filesize [x]
-    (.length x))
-  (->bytes [x]
-    (->bytes (io/input-stream x)))
-  (->base64 [x]
-    (->base64 (->bytes x)))
-  (detect-image-format [x]
-    (detect-image-format (io/input-stream x)))
-  (mime-type [x]
-    (Files/probeContentType (.toPath x)))
+  (filesize [x] (.length x))
+  (->bytes [x] (->bytes (io/input-stream x)))
+  (->base64 [x] (->base64 (->bytes x)))
+  (detect-image-format [x] (detect-image-format (io/input-stream x)))
+  (mime-type [x] (Files/probeContentType (.toPath x)))
   (filename [x] (.getName x))
   (write
     ([x] (write x (System/getProperty "java.io.tmpdir")))
-    ([x dest]
-     (let [name (filename x)]
-       (write (->bytes x) (str dest "/" name)))))
+    ([x dest] (write x (filename x) dest))
+    ([x n dest] (write (->bytes x) (str dest "/" n))))
   (scale
-    ([x]
-     (scale x 750 750))
-    ([x width height]
-     (scale x width height speed))
-    ([x width height quality]
-     (scale (io/input-stream x) width height quality) ))
+    ([x] (scale x 750 750))
+    ([x width height] (scale x width height speed))
+    ([x width height quality] (scale (io/input-stream x) width height quality)))
+
   URI
-  (filesize [x]
-    (Long/parseLong (get-in (client/head (str x)) [:headers "Content-Length"])))
+  (filesize [x] (Long/parseLong (get-in (client/head (str x)) [:headers "Content-Length"])))
   (filename [x] (filename (io/file (.getPath x))))
-  URL
-  (filesize [x]
-    (Long/parseLong (get-in (client/head (str x)) [:headers "Content-Length"])))
-  (filename [x] (filename (io/file (.getPath x))))
-  (->bytes [x]
-    (:body (client/get (str x) {:as :byte-array})))
+  (->bytes [x] (->bytes (.toURL x)))
+  (->base64 [x] (->base64 (.toURL x)))
+  (detect-image-format [x] (detect-image-format (.toURL x)))
+  (mime-type [x] (mime-type (.toURL x)))
   (write
     ([x] (write x (System/getProperty "java.io.tmpdir")))
-    ([x folder] (let [name (filename x)]
-                  (write (->bytes x) (str folder "/" name)))))
+    ([x dest] (write x (filename x) dest))
+    ([x n dest] (write (->bytes x) (str dest "/" n))))
   (scale
-    ([x]
-     (scale x 750 750))
-    ([x width height]
-     (scale x width height speed))
-    ([x width height quality]
-     (scale (:body (client/get (str x) {:as :byte-array})) width height quality)))
+    ([x] (scale x 750 750))
+    ([x width height] (scale x width height speed))
+    ([x width height quality] (scale (.toURL x) width height quality)))
+  URL
+  (filesize [x] (Long/parseLong (get-in (client/head (str x)) [:headers "Content-Length"])))
+  (filename [x] (filename (io/file (.getPath x))))
+  (->bytes [x] (:body (client/get (str x) {:as :byte-array})))
+  (mime-type [x] (-> x .openConnection .getContentType))
+  (write
+    ([x] (write x (System/getProperty "java.io.tmpdir")))
+    ([x dest] (write x (filename x) dest))
+    ([x n dest] (write (->bytes x) (str dest "/" n))))
+  (scale
+    ([x] (scale x 750 750))
+    ([x width height] (scale x width height speed))
+    ([x width height quality] (scale (:body (client/get (str x) {:as :byte-array})) width height quality)))
+
   String
   (filesize [x]
     (cond
       (.exists (io/file x)) (filesize (io/file x))
-      (URL. x) (filesize (URL. x))))
+      (url-string? x) (filesize (URL. x))
+      :else (throw (ex-info "Cannot determine filesize" {:input x}))))
   (filename [x]
     (cond
       (.exists (io/file x)) (filename (io/file x))
-      (URL. x) (filename (URL. x))))
+      (url-string? x) (filename (URL. x))
+      :else (throw (ex-info "Cannot determine filename" {:input x}))))
   (->bytes [x]
     (cond
       (.exists (io/file x)) (->bytes (io/input-stream x))
-      (URL. x) (->bytes (URL. x))))
+      (url-string? x) (->bytes (URL. x))
+      :else (throw (ex-info "Cannot determine bytes" {:input x}))))
+  (->base64 [x] (->base64 (->bytes x)))
+  (detect-image-format [x] (detect-image-format (io/input-stream x)))
+  (mime-type [x]
+    (when (.exists (io/file x)) (Files/probeContentType (.toPath (io/file x)))))
+  (write
+    ([x] (write x (System/getProperty "java.io.tmpdir")))
+    ([x dest] (write x (filename x) dest))
+    ([x n dest]
+     (cond
+       (.exists (io/file x)) (write (->bytes (io/file x)) (str dest "/" n))
+       (url-string? x) (write (URL. x) n dest)
+       :else (throw (ex-info "Cannot write" {:input x})))))
   (scale
-    ([x]
-     (scale x 750 750))
-    ([x width height]
-     (scale x width height speed))
+    ([x] (scale x 750 750))
+    ([x width height] (scale x width height speed))
     ([x width height quality]
      (cond
        (.exists (io/file x)) (scale (io/file x) width height quality)
-       (URL. x) (scale (URL. x) width height quality))))
-  (->base64 [x]
-    (->base64 (->bytes x)))
-  (detect-image-format [x]
-    (detect-image-format (io/input-stream x)))
-  (write
-    ([x] (write x (System/getProperty "java.io.tmpdir")))
-    ([x dest]
-     (cond
-       (.exists (io/file x)) (write (->bytes (io/file x)) (str dest "/" (filename x)))
-       (URL. x) (write (URL. x) dest))))
-  (mime-type [x]
-    (when (.exists (io/file x)) (Files/probeContentType (.toPath (io/file x)))))
+       (url-string? x) (scale (URL. x) width height quality)
+       :else (throw (ex-info "Cannot scale" {:input x})))))
+
   byte/1
-  (->base64 [x]
-    (crypto/encode-base64 x))
-  (detect-image-format [x]
-    (detect-image-format (io/input-stream x)))
+  (->base64 [x] (crypto/encode-base64 x))
+  (detect-image-format [x] (detect-image-format (io/input-stream x)))
   (write
-    ([x] (write x (System/getProperty "java.io.tmpdir")))
+    ([x] (throw (ex-info "Cannot write byte array with 1-arity: no filename available. Use (write x dest) or (write x n dest)" {})))
     ([x dest]
      (with-open [w (io/output-stream dest)]
+       (.write w x)))
+    ([x n dest]
+     (with-open [w (io/output-stream (str dest "/" n))]
        (.write w x))))
   (scale
-    ([x]
-     (scale x 750 750))
-    ([x width height]
-     (scale x width height speed))
-    ([x width height quality]
-     (scale (io/input-stream x) width height quality)))
+    ([x] (scale x 750 750))
+    ([x width height] (scale x width height speed))
+    ([x width height quality] (scale (io/input-stream x) width height quality)))
+
   BufferedInputStream
   (->bytes [x]
     (with-open [in x
                 out (java.io.ByteArrayOutputStream.)]
-    (io/copy in out)
+      (io/copy in out)
       (.toByteArray out)))
   (detect-image-format [x]
-    (let [in (ImageIO/createImageInputStream x)
-        iis (ImageIO/getImageReaders in)]
-    (for [reader (iterator-seq iis)]
-      (.getFormatName reader))))
+  (with-open [in (ImageIO/createImageInputStream x)]
+    (doall
+      (for [reader (iterator-seq (ImageIO/getImageReaders in))]
+        (.getFormatName reader)))))
   (clone [is n]
     (let [baos (ByteArrayOutputStream.)]
       (.transferTo is baos)
       (take n (repeatedly #(io/input-stream (.toByteArray baos))))))
+  (write
+    ([x] (throw (ex-info "Cannot write BufferedInputStream with 1-arity: no filename available. Use (write x dest) or (write x n dest)" {})))
+    ([x dest] (write (->bytes x) dest))
+    ([x n dest] (write (->bytes x) n dest)))
   (scale
-    ([x]
-     (scale x 750 750))
-    ([x width height]
-     (scale x width height speed))
+    ([x] (scale x 750 750))
+    ([x width height] (scale x width height speed))
     ([x width height quality]
      (let [[a b c] (clone x 3)
-           image-format (first (detect-image-format a))
-           transformation-f (case (orientation c)
-                              "Left side, bottom (Rotate 270 CW)" (comp (rotate-90-counter-clockwise-fn) (resize-fn width height quality))
-                              "Top, left side (Horizontal / normal)" (resize-fn width height quality)
-                              (resize-fn width height quality))
+           image-format      (first (detect-image-format a))
+           transformation-f  (case (orientation c)
+                               "Left side, bottom (Rotate 270 CW)"      (comp (rotate-90-counter-clockwise-fn) (resize-fn width height quality))
+                               "Top, left side (Horizontal / normal)"   (resize-fn width height quality)
+                               (resize-fn width height quality))
            ^BufferedImage resized-image (transformation-f b)]
        (with-open [baos (java.io.ByteArrayOutputStream.)]
          (ImageIO/write resized-image image-format baos)
-         (.toByteArray baos)))))
-  (write
-    ([x] (write x (System/getProperty "java.io.tmpdir")))
-    ([x dest]
-     (write (->bytes x) dest))))
+         (.toByteArray baos))))))
+
 
--- test/beeld/core_test.clj
+++ test/beeld/core_test.clj
@@ -1,2 +1,238 @@
-(ns beeld.core-test)
+(ns beeld.core-test
+  (:require [clojure.test :refer :all]
+            [clojure.string :as str]
+            [clojure.java.io :as io]
+            [image-resizer.scale-methods :refer [speed]]
+            [beeld.core :as sut])
+  (:import [java.io File BufferedInputStream ByteArrayOutputStream]
+           [java.net URI URL]
+           [java.awt.image BufferedImage]
+           [javax.imageio ImageIO]))
 
+(def test-dir (str (System/getProperty "java.io.tmpdir") "/beeld-test/"))
+
+(defn test-dir-fixture [f]
+  (.mkdirs (File. test-dir))
+  (f))
+
+(use-fixtures :once test-dir-fixture)
+
+(defn create-test-png []
+  (let [bi   (BufferedImage. 100 100 BufferedImage/TYPE_INT_ARGB)
+        baos (ByteArrayOutputStream.)]
+    (ImageIO/write bi "png" baos)
+    (.toByteArray baos)))
+
+(defn create-test-jpg []
+  (let [bi   (BufferedImage. 100 100 BufferedImage/TYPE_INT_RGB)
+        baos (ByteArrayOutputStream.)]
+    (ImageIO/write bi "jpg" baos)
+    (.toByteArray baos)))
+
+(defn write-test-file [path content]
+  (with-open [fos (io/output-stream path)]
+    (.write fos content)))
+
+;;; url-string?
+
+(deftest url-string-helper
+  (testing "valid http/https URLs pass — have both protocol and host"
+    (is (sut/url-string? "http://example.com/image.png"))
+    (is (sut/url-string? "https://example.com/image.png")))
+  (testing "invalid inputs fail"
+    (is (not (sut/url-string? "not a url")))
+    (is (not (sut/url-string? ""))))
+  (testing "file:// URIs fail — no host component"
+    (is (not (sut/url-string? "file:///path/to/file.png")))))
+
+;;; filename
+
+(deftest filename-protocol
+  (testing "File"
+    (let [f (io/file test-dir "test.png")]
+      (spit f "")
+      (is (= "test.png" (sut/filename f)))
+      (io/delete-file f)))
+  (testing "String path"
+    (let [f (str test-dir "test_str.png")]
+      (spit f "")
+      (is (= "test_str.png" (sut/filename f)))
+      (io/delete-file f)))
+  (testing "String invalid throws"
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (sut/filename "not-a-file-or-url"))))
+  (testing "URI"
+    (let [u (URI. "file:/test_uri.png")]
+      (is (string? (sut/filename u)))))
+  (testing "URL"
+    (let [u (URL. "file:/test_url.png")]
+      (is (string? (sut/filename u))))))
+
+;;; filesize
+
+(deftest filesize-protocol
+  (testing "File"
+    (let [f       (io/file test-dir "test_size.png")
+          content "hello world"]
+      (spit f content)
+      (is (= (count content) (sut/filesize f)))
+      (io/delete-file f)))
+  (testing "String path"
+    (let [f       (str test-dir "test_size_str.png")
+          content "hello world"]
+      (spit f content)
+      (is (= (count content) (sut/filesize f)))
+      (io/delete-file f)))
+  (testing "String invalid throws"
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (sut/filesize "not-a-file-or-url")))))
+
+;;; ->bytes
+
+(deftest ->bytes-protocol
+  (testing "File"
+    (let [f       (io/file test-dir "test_bytes.png")
+          content (create-test-png)]
+      (write-test-file f content)
+      (is (bytes? (sut/->bytes f)))
+      (io/delete-file f)))
+  (testing "String path"
+    (let [f       (str test-dir "test_bytes_str.png")
+          content (create-test-png)]
+      (write-test-file f content)
+      (is (bytes? (sut/->bytes f)))
+      (io/delete-file f)))
+  (testing "String invalid throws"
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (sut/->bytes "not-a-file-or-url")))))
+
+;;; ->base64
+
+(deftest ->base64-protocol
+  (testing "byte array"
+    (is (string? (sut/->base64 (create-test-png))))))
+
+;;; detect-image-format
+
+(deftest detect-image-format-protocol
+  (testing "byte array PNG"
+    (is (= ["png"] (map str/lower-case (sut/detect-image-format (create-test-png))))))
+  (testing "byte array JPG"
+    (is (= ["jpeg"] (map str/lower-case (sut/detect-image-format (create-test-jpg))))))
+  (testing "BufferedInputStream PNG"
+    (let [bis (BufferedInputStream. (java.io.ByteArrayInputStream. (create-test-png)))]
+      (is (= ["png"] (map str/lower-case (sut/detect-image-format bis))))))
+  (testing "File PNG"
+    (let [f (io/file test-dir "test_fmt.png")]
+      (write-test-file f (create-test-png))
+      (is (= ["png"] (map str/lower-case (sut/detect-image-format f))))
+      (io/delete-file f))))
+
+;;; mime-type
+
+(deftest mime-type-protocol
+  (testing "File"
+    (let [f (io/file test-dir "test_mime.png")]
+      (write-test-file f (create-test-png))
+      (is (some? (sut/mime-type f)))
+      (io/delete-file f)))
+  (testing "String path"
+    (let [f (str test-dir "test_mime_str.png")]
+      (write-test-file f (create-test-png))
+      (is (some? (sut/mime-type f)))
+      (io/delete-file f))))
+
+;;; write
+
+(deftest write-protocol
+  (testing "byte array - 1-arity throws"
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (sut/write (create-test-png)))))
+  (testing "byte array - 2-arity to full dest path"
+    (let [content (create-test-png)
+          dest    (io/file test-dir "test_write.png")]
+      (sut/write content (str dest))
+      (is (.exists dest))
+      (io/delete-file dest)))
+  (testing "byte array - 3-arity with name and folder"
+    (let [content (create-test-png)
+          dest    (io/file test-dir "test_write_dir")]
+      (.mkdirs dest)
+      (sut/write content "write_test.png" (str dest))
+      (is (.exists (io/file dest "write_test.png")))
+      (io/delete-file dest :recursive true)))
+  (testing "BufferedInputStream - 1-arity throws"
+    (let [bis (BufferedInputStream. (java.io.ByteArrayInputStream. (create-test-png)))]
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (sut/write bis)))))
+  (testing "BufferedInputStream - 2-arity to full dest path"
+    (let [bis  (BufferedInputStream. (java.io.ByteArrayInputStream. (create-test-png)))
+          dest (str test-dir "test_write_bis.png")]
+      (sut/write bis dest)
+      (is (.exists (io/file dest)))
+      (io/delete-file dest)))
+  (testing "BufferedInputStream - 3-arity with name and folder"
+    (let [bis  (BufferedInputStream. (java.io.ByteArrayInputStream. (create-test-png)))
+          dest (io/file test-dir "test_write_bis_dir")]
+      (.mkdirs dest)
+      (sut/write bis "bis_written.png" (str dest))
+      (is (.exists (io/file dest "bis_written.png")))
+      (io/delete-file dest :recursive true)))
+  (testing "File - 3-arity with name and folder"
+    (let [f    (io/file test-dir "test_write_file.png")
+          dest (io/file test-dir "test_write_file_dest")]
+      (write-test-file f (create-test-png))
+      (.mkdirs dest)
+      (sut/write f "written.png" (str dest))
+      (is (.exists (io/file dest "written.png")))
+      (io/delete-file f)
+      (io/delete-file dest :recursive true)))
+  (testing "String - 3-arity with name and folder"
+    (let [f    (str test-dir "test_write_str.png")
+          dest (io/file test-dir "test_write_str_dest")]
+      (write-test-file f (create-test-png))
+      (.mkdirs dest)
+      (sut/write f "written.png" (str dest))
+      (is (.exists (io/file dest "written.png")))
+      (io/delete-file f)
+      (io/delete-file dest :recursive true)))
+  (testing "String invalid throws"
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (sut/write "not-a-file-or-url" "name.png" test-dir)))))
+
+;;; scale
+
+(deftest scale-protocol
+  (testing "byte array - default 750x750"
+    (is (bytes? (sut/scale (create-test-png)))))
+  (testing "byte array - explicit dimensions"
+    (is (bytes? (sut/scale (create-test-png) 50 50))))
+  (testing "byte array - explicit dimensions and quality"
+    (is (bytes? (sut/scale (create-test-png) 50 50 speed))))
+  (testing "File"
+    (let [f (io/file test-dir "test_scale_file.png")]
+      (write-test-file f (create-test-png))
+      (is (bytes? (sut/scale f 50 50)))
+      (io/delete-file f)))
+  (testing "String path"
+    (let [f (str test-dir "test_scale_str.png")]
+      (write-test-file f (create-test-png))
+      (is (bytes? (sut/scale f 50 50)))
+      (io/delete-file f)))
+  (testing "String invalid throws"
+    (is (thrown? clojure.lang.ExceptionInfo
+                 (sut/scale "not-a-file-or-url" 50 50))))
+  (testing "BufferedInputStream"
+    (let [bis (BufferedInputStream. (java.io.ByteArrayInputStream. (create-test-png)))]
+      (is (bytes? (sut/scale bis 50 50))))))
+
+;;; clone
+
+(deftest clone-protocol
+  (testing "BufferedInputStream produces n independent streams"
+    (let [content (create-test-png)
+          bis     (BufferedInputStream. (java.io.ByteArrayInputStream. content))
+          cloned  (sut/clone bis 3)]
+      (is (= 3 (count cloned)))
+      (is (every? #(instance? java.io.InputStream %) cloned))
+      (is (every? #(pos? (.available %)) cloned)))))
